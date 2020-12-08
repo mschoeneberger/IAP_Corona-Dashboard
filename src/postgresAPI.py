e842029f90f33e1ea75b249221131896ec28202b
Maximilian Schöneberger <maxschoeneberger@t-online.de>
2020-12-08 18:35:02 +0100

    func for loading data in DB, config for DB-connect

--- src/postgresAPI.py
+++ src/postgresAPI.py
@@ -1,30 +1,72 @@
 import psycopg2
+import os
+import json
+from config import config
+from scrapeData import getJsonData
 
-def main():
-    connection = psycopg2.connect(
-        host = "localhost",
-        database="Corona-Dashboard",
-        user="postgres" ,
-        password="postgres")
 
-    cur = connection.cursor()
-
-    cur.execute('SELECT version()')
-    db_version = cur.fetchone()
-
-    command = """
+def formatData_germany():
+    output = []
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    path = current_dir + '/../storage/germany.json'
+    data = open(path, 'r')
+    json_iter = json.loads(data.read())["features"]
     
-    """
+    for element in json_iter:
+        element_dict = element["properties"]
+        row = [None] * 15
+        row[0] = element_dict["IdBundesland"]
+        row[1] = element_dict["Bundesland"]
+        row[2] = element_dict["IdLandkreis"]
+        row[3] = element_dict["Landkreis"]
+        row[4] = element_dict["Geschlecht"]
+        row[5] = element_dict["Altersgruppe"]
+        row[6] = element_dict["AnzahlFall"]
+        row[7] = element_dict["AnzahlTodesfall"]
+        row[8] = element_dict["Meldedatum"]
+        row[9] = element_dict["NeuerFall"]
+        row[10] = element_dict["Refdatum"]
+        row[11] = element_dict["AnzahlGenesen"]
+        row[12] = element_dict["NeuGenesen"]
+        row[13] = bool(element_dict["IstErkrankungsbeginn"])
+        row[14] = element_dict["ObjectId"]
+        output.append(tuple(row))
 
-    cur.execute(command)
+    return output
 
-    print(db_version)
 
-    cur.close()
+def insertIntoDatabase(data):
 
-    connection.commit()
-if __name__ == "__main__":
+    sql = """INSERT INTO germany VALUES{}"""
 
-    main()
+    conn = None
+    try:
+        params = config() # read database configuration
+        conn = psycopg2.connect(**params) # connect to the PostgreSQL database
+        cur = conn.cursor() # create a new cursor
 
+        # execute the INSERT statement
+        for row in data:
+            sql_final = sql.format(str(row))
+            cur.execute(sql_final)
+            
+        conn.commit() # commit the changes to the database
+        cur.close()  # close communication with the database
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+# url for german dataset: 'https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.geojson'
+
+def updateTables():
+    getJsonData('germany', 'https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.geojson')
+    german_data = formatData_germany()
+    insertIntoDatabase(german_data)
+
+
+if __name__ == '__main__':
+    updateTables()
+    
     print("You are doing great! :)")
--- src/scrapeData.py
+++ src/scrapeData.py
@@ -15,22 +15,13 @@ def connect_to_endpoint(url):
     return response.json()
 
 
-def getJSONData(label, url):
+def getJsonData(label, url):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     path = current_dir + '/../storage/' + label + '.json'
     data = connect_to_endpoint(url)
 
     with open(path, 'w') as output:
-        output.write(json.dumps(data, indent=4, sort_keys=True))
-
-
-# url for full german dataset https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.geojson
-# dont know if this link stays the same over time
-
-
-def main():
-    getJSONData("germany", 'https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.geojson')
+        output.write(json.dumps(data, indent=4))
 
 if __name__ == "__main__":
-    main()
     print("You are doing great! :)")
