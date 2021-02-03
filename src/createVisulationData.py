ff18ee23942436a98016f21dbefba019c85414ff
Maximilian Schöneberger <maxschoeneberger@t-online.de>
2021-02-03 01:34:06 +0100

    world visulation data implemented

--- src/createVisulationData.py
+++ src/createVisulationData.py
@@ -1,11 +1,52 @@
+from retrieveData import getJsonData
 import json
-from postgresAPI import executeSelect
+import os
+
+
+CURRENT_DIR = current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# "confirmed": 6318,
+#             "deaths": 122,
+#             "recovered": 0,
+#             "active": 0,
+#             "incident_rate": "72.31008873379518",
+#             "people_tested": "",
+#             "people_hospitalized": "",
+#             "report_date": "2020-04-19",
+#             "delta_confirmed": "324",
+#             "delta_recovered": "",
+#             "country": "Serbia"
 
 
 def createWorldData():
-    data = executeSelect('world', '*')
+
+    countryIsoCodes = json.load(open(CURRENT_DIR + '/../storage/country_iso2.json'))
+    world = []
+
+    # download files
+    for country in countryIsoCodes:
+        try:
+            #getJsonData(country['name'], 'https://mahabub81.github.io/covid-19-api/api/v1/countries/' + country['alpha-2'] + '.json')
+            
+            with open(CURRENT_DIR + '/../storage/' + country['name'] + '.json') as file:
+                json_file = json.load(file)
+                for element in json_file:
+                    element["country"] = country['name']
+                    del element["last_updated_at"]
+                    world.append(element)
+
+
+        except:
+            print(country)
+
+    with open(CURRENT_DIR + '/../api_files/world.json', 'w') as outfile:
+        json.dump(world, outfile, indent=4)
+
+    
+
+
 
 
 
 if __name__ == "__main__":
-    main()
+    createWorldData()
--- src/flaskEndpoint.py
+++ src/flaskEndpoint.py
@@ -1,36 +1,22 @@
 from flask import Flask, request, send_from_directory
 from flask_cors import CORS
-from postgresAPI import executeSelect
 import json
+import os
+
+CURRENT_DIR = current_dir = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
 CORS(app)
 
-# /fetchData?table=germany&startdate=10.02.2020&enddate=15.02.2020&values=cases
-@app.route('/fetchData')
-def fetchData():
-    #if key doesn't exist, returns None
-    table = request.args.get('table')
-    values = request.args.get('values')
-    group_by = request.args.get('group_by')
-
-    data = executeSelect(table, values, group_by)
-
-    # try to return json, return pure data if not json-able (error in SQL)
-    # TODO: convert date to string
-    try:
-        return json.dumps(data)
-    except:
-        return "ERROR:" + str(data)
 
 @app.route('/dataWorld')
 def dataWorld():
-    ...
+    
+    file = open(CURRENT_DIR + '/../api_files/world.json')
+
+    return file
 
 
 @app.route('/')
 def default():
     return '''<h1>IAP_Corona_Dashboard - Flask-Endpoint</h1>'''
-
-if __name__ == "__main__":
-    ...
