b1a914040ce6c092a234cd64ea42d13c59b73c2a
Maximilian Schöneberger <maxschoeneberger@t-online.de>
2021-03-02 16:17:27 +0100

    added many countries

--- src/createVisulationData.py
+++ src/createVisulationData.py
@@ -1,5 +1,6 @@
 import retrieveData as rd
 import json
+import requests
 import os
 import copy
 import pandas as pd
@@ -17,7 +18,7 @@ def createWorldData():
     # download files
     for country in countryIsoCodes:
         try:
-            rd.getJsonData(country['name'], 'https://mahabub81.github.io/covid-19-api/api/v1/countries/' + country['alpha-2'] + '.json')
+            rd.getJsonData("world/" + country['name'], 'https://mahabub81.github.io/covid-19-api/api/v1/countries/' + country['alpha-2'] + '.json')
             
             world[country["name"]] = []
             with open(CURRENT_DIR + '/../storage/world/' + country['name'] + '.json') as file:
@@ -99,10 +100,11 @@ def createNetherlandsData():
     with open(CURRENT_DIR + '/../api_files/netherlands.json', 'w') as outfile:
         json.dump(output, outfile, indent=4)
 
-# TODO
+
 def createBelgiumData():
     label = "belgium"
 
+    # download data
     rd.getCsvData(label + "/cases", 'https://epistat.sciensano.be/Data/COVID19BE_CASES_AGESEX.csv')
     rd.getCsvData(label + "/deaths", 'https://epistat.sciensano.be/Data/COVID19BE_MORT.csv')
     rd.getCsvData(label + "/hospital", 'https://epistat.sciensano.be/Data/COVID19BE_HOSP.csv')
@@ -118,45 +120,44 @@ def createBelgiumData():
     hospital = pd.read_csv(path + "hospital.csv")
     tested = pd.read_csv(path + "tests.csv")
 
-    row = [None] * 8
+    row = {}
     olddate = '1970-01-01'
 
 
     for index, df_row in cases.iterrows():
         if index != 0:
-            if (df_row["DATE"] == row[0]) and (df_row["PROVINCE"] == row[1]):
-                row[3] += df_row["CASES"]
+            if (df_row["DATE"] == row["date"]) and (df_row["PROVINCE"] == row["region"]):
+                row["cases"] += df_row["CASES"]
                 continue
             else:
-                #row = [float("nan") if e is None for e in row]
-                output.append(tuple(row))
+                output.append(row)
         
-        row = [None] * 8
-        row[0] = df_row["DATE"]
-        row[1] = df_row["PROVINCE"]
-        row[3] = df_row["CASES"]
+        row = {}
+        row["date"] = df_row["DATE"]
+        row["region"] = df_row["PROVINCE"]
+        row["cases"] = df_row["CASES"]
         try:
-            row[2] = int(tested.loc[(tested["DATE"] == row[0]) & (tested["PROVINCE"] == row[1])]["TESTS_ALL"])
+            row["tested"] = int(tested.loc[(tested["DATE"] == row["date"]) & (tested["PROVINCE"] == row["region"])]["TESTS_ALL"])
         except:
-            row[2] = float("nan")
+            row["tested"] = None
         try:
-            row[4] = int(hospital.loc[(hospital["DATE"] == row[0]) & (hospital["PROVINCE"] == row[1])]["NEW_IN"])
-            row[5] = int(hospital.loc[(hospital["DATE"] == row[0]) & (hospital["PROVINCE"] == row[1])]["TOTAL_IN"])
-            row[6] = int(hospital.loc[(hospital["DATE"] == row[0]) & (hospital["PROVINCE"] == row[1])]["TOTAL_IN_ICU"])
+            row["new_hosp"] = int(hospital.loc[(hospital["DATE"] == row["date"]) & (hospital["PROVINCE"] == row["region"])]["NEW_IN"])
+            row["current_hosp"] = int(hospital.loc[(hospital["DATE"] == row["date"]) & (hospital["PROVINCE"] == row["region"])]["TOTAL_IN"])
+            row["current_icu"] = int(hospital.loc[(hospital["DATE"] == row["date"]) & (hospital["PROVINCE"] == row["region"])]["TOTAL_IN_ICU"])
         except:
-            row[4] = float("nan")
-            row[5] = float("nan")
-            row[6] = float("nan")
-        if row[0] != olddate:
-            row[7] = deaths.loc[deaths["DATE"] == row[0]]["DEATHS"].sum()
-            olddate = row[0]
+            row["new_hosp"] = None
+            row["current_hosp"] = None
+            row["current_icu"] = None
+        if row["date"] != olddate:
+            row["deceased"] = int(deaths.loc[deaths["DATE"] == row["date"]]["DEATHS"].sum())
+            olddate = row["date"]
         else:
-            row[7] = float("nan")
+            row["deceased"] = None
 
-    #row = [float("nan") if e is None for e in row]
-    output.append(tuple(row))
+    output.append(row)
     
-    return output
+    with open(CURRENT_DIR + '/../api_files/belgium.json', 'w') as outfile:
+        json.dump(output, outfile, indent=4)
 
 
 def createLuxembourgData():
@@ -256,24 +257,181 @@ def createSwitzerlandData():
         output.append(row)
 
     with open(CURRENT_DIR + '/../api_files/switzerland.json', 'w') as outfile:
-        json.dump(output, outfile, indent=4)    
+        json.dump(output, outfile, indent=4)
 
+
+def createAustriaData():
+    #download data
+    rd.indirectLinkCsv("austria" + "_fz", 'https://covid19-dashboard.ages.at/data/CovidFaelle_Timeline.csv')
+    rd.indirectLinkCsv("austria" + "_hosp", "https://covid19-dashboard.ages.at/data/CovidFallzahlen.csv")
+
+    #format data
+    output = []
+    path = CURRENT_DIR + '/../storage/austria'
+    data = pd.read_csv(path + "_fz.csv", sep=';')
+    data_hosp = pd.read_csv(path + "_hosp.csv", sep=';')
+    olddate = "1970-01-01"
+    olddate_value = 0
+
+    
+    for index, df_row in data.iterrows():
+        if df_row["BundeslandID"] == 10:
+            continue
+        row = {}
+        row["date"] = datetime.strptime(df_row["Time"], '%d.%m.%Y %X').strftime('%Y-%m-%d')
+        row["region"] = df_row["Bundesland"]
+        if row["date"] != olddate:
+            try:
+                new_value = int(data_hosp.loc[(data_hosp["Meldedat"] == df_row["Time"][:10]) & (data_hosp["Bundesland"] == "Alle")]["TestGesamt"])
+                row["tested"] = new_value - olddate_value
+                olddate_value = new_value
+            except:
+                row["tested"] = None
+        row["cases"] = df_row["AnzahlFaelle"]
+        try:
+            row["current_hosp"] = int(data_hosp.loc[(data_hosp["Meldedat"] == df_row["Time"][:10]) & (data_hosp["Bundesland"] == df_row["Bundesland"])]["FZHosp"])
+            row["current_icu"] = int(data_hosp.loc[(data_hosp["Meldedat"] == df_row["Time"][:10]) & (data_hosp["Bundesland"] == df_row["Bundesland"])]["FZICU"])
+        except:
+            row["current_hosp"] = None
+            row["current_icu"] = None
+        row["recovered"] = df_row["AnzahlGeheiltTaeglich"]
+        row["deceased"] = df_row["AnzahlTotTaeglich"]
+
+        output.append(row)
+    
+    with open(CURRENT_DIR + '/../api_files/austria.json', 'w') as outfile:
+        json.dump(output, outfile, indent=4)
+
+
+def download_file(label, url):
+    chunk_size = int(os.environ.get("CHUNK_SIZE"))
+    # NOTE the stream=True parameter below
+    print("downloading", label, "in chunks of size", chunk_size, "...")
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(CURRENT_DIR + '/../storage/' + label, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=chunk_size): 
+                # If you have chunk encoded response uncomment if
+                # and set chunk_size parameter to None.
+                #if chunk: 
+                f.write(chunk)
+
+
+def createGermanyData():
+    # download_file("germany.json", 'https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.geojson')
+    rd.getJsonData("germany", 'https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.geojson')
+
+    output = []
+    path = CURRENT_DIR + '/../storage/germany.json'
+    data = open(path, 'r')
+    json_iter = json.loads(data.read())["features"]
+
+    counter = 0
+    for element in json_iter:
+        if (counter % 20 == 0):
+            print(counter)
+        element_dict = element["properties"]
+        row = {}
+        row["date"] = element_dict["Meldedatum"][:10]
+        row["bundesland"] = element_dict["Bundesland"]
+        row["landkreis"] = element_dict["Landkreis"]
+        row["geschlecht"] = element_dict["Geschlecht"]
+        row["altergruppe"] = element_dict["Altersgruppe"]
+        row["cases"] = element_dict["AnzahlFall"]
+        row["deceased"] = element_dict["AnzahlTodesfall"]
+        row["recovered"] = element_dict["AnzahlGenesen"]
+        output.append(row)
+        counter += 1
+
+    with open(CURRENT_DIR + '/../api_files/germany.json', 'w') as outfile:
+        json.dump(output, outfile, indent=4)
+
+
+def createGermanyITSData():
+    rd.getCsvData("germany_its", "https://diviexchange.blob.core.windows.net/%24web/bundesland-zeitreihe.csv")
+
+    output = []
+    path = CURRENT_DIR + '/../storage/germany_its.csv'
+    data = pd.read_csv(path)
+
+    for index, df_row in data.iterrows():
+        row = {}
+
+        row["date"] = df_row["Datum"][:10]
+        row["bundesland"] = df_row["Bundesland"]
+        row["cases"] = df_row["Aktuelle_COVID_Faelle_Erwachsene_ITS"]
+        row["belegte_betten"] = df_row["Belegte_Intensivbetten_Erwachsene"]
+        row["freie_betten"] = df_row["Freie_Intensivbetten_Erwachsene"]
+        row["freie_iv"] = df_row["Freie_IV_Kapazitaeten_Gesamt"]
+        row["freie_iv_covid"] = df_row["Freie_IV_Kapazitaeten_Davon_COVID"]
+
+        output.append(row)
+    
+    with open(CURRENT_DIR + '/../api_files/germany_its.json', 'w') as outfile:
+        json.dump(output, outfile, indent=4)
+
+
+def createGermanyVaccData():
+
+    rd.indirectLinkXlsx("germany_vacc", "https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquotenmonitoring.xlsx?__blob=publicationFile")
+
+    output = []
+    path = CURRENT_DIR + '/../storage/germany_vacc.xlsx'
+
+    xlsx = pd.ExcelFile(path , engine='openpyxl')
+    data_region = pd.read_excel(xlsx, 1)
+    data_daily = pd.read_excel(xlsx, 3)
+
+    data_region.dropna(inplace=True)
+    #data_region.drop(data_daily.tail(1).index,inplace=True)
+
+    for index, df_row in data_region.iterrows():
+        row = {}
+
+        row["bundesland"] = df_row["Bundesland"]
+        row["total vacc"] = df_row["Gesamtzahl bisher verabreichter Impfstoffdosen"]
+        # row["diff zum vortag"] = df_row["Differenz zum Vortag"]
+
+        output.append(row)
+
+    data_daily.dropna(inplace=True)
+    data_daily.drop(data_daily.tail(1).index,inplace=True)
+
+    for index, df_row in data_daily.iterrows():
+        row = {}
+        row["date"] = str(df_row["Datum"])[:10]
+        row["first vacc"] = df_row["Erstimpfung"]
+        row["second vacc"] = df_row["Zweitimpfung"]
+        row["total"] = df_row["Gesamtzahl verabreichter Impfstoffdosen"]
+        output.append(row)
+
+    with open(CURRENT_DIR + '/../api_files/germany_vacc.json', 'w') as outfile:
+        json.dump(output, outfile, indent=4)
 
 # TODO:
-# Belgium
-# Austria
 # Tschechien
 # Poland
+# Germany Vacc
+# denmark data not up to date ?!?
 
 if __name__ == "__main__":
-    createSwitzerlandData()
+    # createAustriaData()
+    # createBelgiumData()
+    # createDenmarkData()
+    # createFranceData()
+    # createLuxembourgData()
+    # createNetherlandsData()
+    # createSwitzerlandData()
+    # createWorldData()
+    # createGermanyData()
+    createGermanyVaccData()
 
-    with open(CURRENT_DIR + '/../api_files/switzerland.json', 'r') as outfile:
-        a = json.load(outfile)
-        summ = 0
-        for b in a:
-            try:
-                summ += b["cases"]
-            except:
-                pass
-        print(summ)
+    # with open(CURRENT_DIR + '/../api_files/germany.json', 'r') as outfile:
+    #     a = json.load(outfile)
+    #     summ = 0
+    #     for b in a:
+    #         try:
+    #             summ += b["cases"]
+    #         except:
+    #             pass
+    #     print(summ)
--- src/flaskEndpoint.py
+++ src/flaskEndpoint.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import json
 import os
 
-from createVisulationData import createWorldData
+from createVisulationData import createWorldData, createGermanyData
 
 CURRENT_DIR = current_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -18,6 +18,13 @@ def downloadWorldData():
 
     return "<h1>success <3</h1>"
 
+@app.route('/downloadGermanyData')
+def downloadGermanyData():
+
+    createGermanyData()
+
+    return "<h1>success <3</h1>"
+
 
 @app.route('/dataWorld')
 def dataWorld():
@@ -25,6 +32,11 @@ def dataWorld():
     file = open(CURRENT_DIR + '/../api_files/world.json')
 
     return json.dumps(json.load(file))
+
+# makes files in /api_files available via their name
+@app.route('/<path:path>')
+def send_static(path):
+    return send_from_directory('../api_files', path)
 
 
 @app.route('/')
